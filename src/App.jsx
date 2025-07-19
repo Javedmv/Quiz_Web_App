7e6715678756614d0466db1d80dd2bef5d2bcb81
Javedmv <javedmv777@gmail.com>
2025-07-20 00:14:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Header from "./components/Header";
+function App() {
+    <>
+        <Header/>
+    </>
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <img src="../assets/quiz-logo.png" alt="quiz logo" />
+                <h1>react quiz</h1>
+            </header>
+        </>
+    )
+}
